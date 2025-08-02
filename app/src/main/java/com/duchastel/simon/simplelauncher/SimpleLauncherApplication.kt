1f171b6577f4c2ab4b5ea5666af67e6d255182ff
Simon Duchastel <simon@duchastel.com>
2025-08-01 23:16:15 -0400

    Add hilt

---
+++ app/src/main/java/com/duchastel/simon/simplelauncher/SimpleLauncherApplication.kt
@@ -0,0 +1,7 @@
+package com.duchastel.simon.simplelauncher
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SimpleLauncherApplication : Application()
