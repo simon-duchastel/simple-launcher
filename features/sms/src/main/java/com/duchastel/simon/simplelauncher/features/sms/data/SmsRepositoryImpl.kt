dd9c396e7d272ab81d7f069d5eb4b6bc4ae09e19
Simon Duchastel <simon@duchastel.com>
2025-08-03 18:08:03 -0400

    Send text working

--- features/sms/src/main/java/com/duchastel/simon/simplelauncher/features/sms/data/SmsRepositoryImpl.kt
+++ features/sms/src/main/java/com/duchastel/simon/simplelauncher/features/sms/data/SmsRepositoryImpl.kt
@@ -1,12 +1,23 @@
 package com.duchastel.simon.simplelauncher.features.sms.data
 
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.content.pm.PackageManager
+import android.os.Build
 import android.telephony.SmsManager
 import com.duchastel.simon.simplelauncher.features.permissions.data.Permission
 import com.duchastel.simon.simplelauncher.features.permissions.data.PermissionsRepository
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
+
 import javax.inject.Inject
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 class SmsRepositoryImpl @Inject internal constructor(
     @ApplicationContext private val context: Context,
@@ -16,6 +27,7 @@ class SmsRepositoryImpl @Inject internal constructor(
     private val smsManager: SmsManager = context.getSystemService(SmsManager::class.java)
     private val packageManager = context.packageManager
 
+    @OptIn(ExperimentalTime::class)
     override suspend fun sendSms(phoneNumber: String, message: String): Boolean {
         val result: Boolean = permissionRepository.requestPermission(Permission.SEND_SMS)
         if (!result) {
@@ -26,14 +38,52 @@ class SmsRepositoryImpl @Inject internal constructor(
             return false
         }
 
-//        smsManager.sendTextMessage(
-//            phoneNumber, // destinationAddress
-//            null,        // scAddress
-//            message,     // text
-//            null,        // sentIntent
-//            null,        // deliveryIntent
-//        )
+        return suspendCancellableCoroutine { cont ->
+            val sentAction =
+                "SMS_SENT_ACTION_${Clock.System.now().epochSeconds}_${phoneNumber.hashCode()}"
+            val requestCode = sentAction.hashCode()
+            val messageId = requestCode.toString()
+            val sentIntent = PendingIntent.getBroadcast(
+                context,
+                requestCode,
+                Intent(sentAction).putExtra("messageId", messageId),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
 
-        return result
+            val receiver = object : BroadcastReceiver() {
+                override fun onReceive(ctx: Context?, intent: Intent?) {
+                    if (intent?.extras?.getString("messageId") != messageId) return
+
+                    val successful = resultCode == Activity.RESULT_OK
+                    context.unregisterReceiver(this)
+                    cont.resume(successful)
+                }
+            }
+            val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                Context.RECEIVER_EXPORTED
+            } else {
+                0
+            }
+            context.registerReceiver(
+                receiver,
+                IntentFilter(sentAction),
+                flags,
+            )
+            smsManager.sendTextMessage(
+                phoneNumber, // destinationAddress
+                null,        // scAddress
+                message,     // text
+                sentIntent,  // sentIntent
+                null         // deliveryIntent
+            )
+            cont.invokeOnCancellation {
+                try {
+                    context.unregisterReceiver(receiver)
+                } catch (_: Exception) {
+                    // swallow the exception since it means the receiver wasn't registered
+                }
+            }
+        }
     }
 }
+
