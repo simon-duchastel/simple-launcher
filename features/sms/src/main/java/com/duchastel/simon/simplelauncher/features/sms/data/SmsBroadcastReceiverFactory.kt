a21185cea5264ad4fb6e3b68418ac409fbfd8773
Simon Duchastel <simon@duchastel.com>
2025-08-03 23:19:50 -0400

    Add coments

--- features/sms/src/main/java/com/duchastel/simon/simplelauncher/features/sms/data/SmsBroadcastReceiverFactory.kt
+++ features/sms/src/main/java/com/duchastel/simon/simplelauncher/features/sms/data/SmsBroadcastReceiverFactory.kt
@@ -4,15 +4,17 @@ import android.app.PendingIntent
 import android.content.BroadcastReceiver
 
 /**
- *
- */
-
-/**
- *
+ * Factory for creating SMS broadcast receivers and the corresponding pending intents.
  */
 interface SmsBroadcastReceiverFactory {
     /**
+     * Creates a [BroadcastReceiver] for handling SMS sent events.
      *
+     * @param messageId The unique ID of the sent message.
+     * @param onSentSmsReceived A callback function to be invoked when the SMS sent broadcast is
+     * received. Provides a boolean indicating successful delivery and the broadcast receiver
+     * itself (useful for unregistering itself).
+     * @return The [BroadcastReceiver] instance which received the notification.
      */
     fun createSentSmsBroadcastReceiver(
         messageId: String,
@@ -20,7 +22,12 @@ interface SmsBroadcastReceiverFactory {
     ): BroadcastReceiver
 
     /**
+     * Creates a [PendingIntent] for the SMS sent broadcast.
      *
+     * @param sentAction The action string for the sent SMS broadcast, used as the
+     * [android.content.Intent] action.
+     * @param messageId The unique ID of the sent message.
+     * @param requestCode A private request code for the sender. Must be globally unique.
      */
     fun createSentSmsPendingIntent(
         sentAction: String,
