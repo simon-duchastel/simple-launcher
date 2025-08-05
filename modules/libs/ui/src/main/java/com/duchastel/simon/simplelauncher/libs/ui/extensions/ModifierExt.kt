be3b1609770982cc43846b113cada16fa2dd6631
Simon Duchastel <simon@duchastel.com>
2025-08-05 01:16:59 -0400

    Move to UI module

---
+++ modules/libs/ui/src/main/java/com/duchastel/simon/simplelauncher/libs/ui/extensions/ModifierExt.kt
@@ -0,0 +1,36 @@
+package com.duchastel.simon.simplelauncher.libs.ui.extensions
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.graphics.graphicsLayer
+
+/**
+ * A modifier that provides a bounce effect when clicked. Inherently includes [clickable].
+ */
+fun Modifier.bounceClickable(onClick: () -> Unit) = composed {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.9f else 1f,
+        animationSpec = tween(durationMillis = 100), label = "bounce_animation"
+    )
+
+    this
+        .graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        }
+        .clickable(
+            interactionSource = interactionSource,
+            indication = null,
+            onClick = onClick
+        )
+}
