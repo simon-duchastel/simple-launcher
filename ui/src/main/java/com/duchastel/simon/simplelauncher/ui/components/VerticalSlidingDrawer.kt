4e4cb1f26796091d67ba6ef870a46aea2055f535
Simon Duchastel <simon@duchastel.com>
2025-08-11 23:58:27 -0400

    feat: make VerticalSlidingDrawer swipeable

--- ui/src/main/java/com/duchastel/simon/simplelauncher/ui/components/VerticalSlidingDrawer.kt
+++ ui/src/main/java/com/duchastel/simon/simplelauncher/ui/components/VerticalSlidingDrawer.kt
@@ -1,11 +1,17 @@
 package com.duchastel.simon.simplelauncher.ui.components
 
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetValue
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberStandardBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,12 +25,26 @@ fun VerticalSlidingDrawer(
             skipHiddenState = false
         )
     )
+    val scope = rememberCoroutineScope()
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
         sheetContent = {
             drawerContent()
         }
     ) {
-        content()
+        Box(
+            modifier = Modifier.pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    val y = dragAmount.y
+                    if (y < 0) {
+                        scope.launch {
+                            scaffoldState.bottomSheetState.expand()
+                        }
+                    }
+                }
+            }
+        ) {
+            content()
+        }
     }
 }
