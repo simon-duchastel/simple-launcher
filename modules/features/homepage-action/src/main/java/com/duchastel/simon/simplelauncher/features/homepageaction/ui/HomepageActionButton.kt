a7159022582bb41b98a8603e3ca8606bfa89e227
Simon Duchastel <simon@duchastel.com>
2025-08-13 00:06:09 -0400

    Add button changes'

--- modules/features/homepage-action/src/main/java/com/duchastel/simon/simplelauncher/features/homepageaction/ui/HomepageActionButton.kt
+++ modules/features/homepage-action/src/main/java/com/duchastel/simon/simplelauncher/features/homepageaction/ui/HomepageActionButton.kt
@@ -1,9 +1,11 @@
 package com.duchastel.simon.simplelauncher.features.homepageaction.ui
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
+import com.duchastel.simon.simplelauncher.features.homepageaction.ui.HomepageActionState.EmojiButtonState
 import com.duchastel.simon.simplelauncher.libs.ui.extensions.bounceClickable
 
 @Composable
@@ -11,10 +13,23 @@ fun HomepageButton(state: HomepageActionState, modifier: Modifier) {
     Text(
         text = state.emoji,
         fontSize = 100.sp,
-        modifier = modifier.bounceClickable(
-            onClick = state.onClick,
-            onDoubleClick = state.onDoubleClick,
-            onLongClick = state.onLongClick,
-        )
+        color = MaterialTheme.colorScheme.onSurface.run {
+            if (state.emojiButtonState is EmojiButtonState.Loading) {
+                copy(alpha = 0.38f)
+            } else {
+                this
+            }
+        },
+        modifier = modifier.run {
+            if (state.emojiButtonState is EmojiButtonState.Ready) {
+                bounceClickable(
+                    onClick = state.emojiButtonState.onClick,
+                    onDoubleClick = state.emojiButtonState.onDoubleClick,
+                    onLongClick = state.emojiButtonState.onLongClick,
+                )
+            } else {
+                this
+            }
+        }
     )
 }
