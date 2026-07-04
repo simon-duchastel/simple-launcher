cfa3326071219bab463ba6765a7d2973ce92ccb0
simon-duchastel <41980219+simon-duchastel@users.noreply.github.com>
2026-07-04 02:51:05 -0400

    Close the app drawer with the back gesture (#81)

--- modules/libs/ui/build.gradle.kts
+++ modules/libs/ui/build.gradle.kts
@@ -29,6 +29,7 @@ android {
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.activity.compose)
     implementation("androidx.compose.material:material-icons-core")
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui)
