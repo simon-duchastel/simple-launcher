2039a3595435a2c6a8792f652c2c1d923d61ba3d
Simon Duchastel <simon@duchastel.com>
2026-07-04 02:40:08 -0400

    Remove elevation from SettingsButton to fix shadow pop during alpha fade

--- modules/libs/ui/src/main/java/com/duchastel/simon/simplelauncher/libs/ui/components/SettingsButton.kt
+++ modules/libs/ui/src/main/java/com/duchastel/simon/simplelauncher/libs/ui/components/SettingsButton.kt
@@ -3,6 +3,7 @@ package com.duchastel.simon.simplelauncher.libs.ui.components
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,6 +18,12 @@ fun SettingsButton(
     FloatingActionButton(
         onClick = onClick,
         modifier = modifier,
+        elevation = FloatingActionButtonDefaults.elevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp,
+            focusedElevation = 0.dp,
+            hoveredElevation = 0.dp,
+        ),
     ) {
         Icon(
             imageVector = Icons.Filled.Settings,
